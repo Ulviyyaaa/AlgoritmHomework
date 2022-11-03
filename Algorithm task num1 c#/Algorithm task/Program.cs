115417d87f44bbba967bda9754c815bda8c4ac4c
Ulviyyaaa <tu7s25j8q@code.edu.az>
2022-11-03 23:56:15 +0400

    algotask

---
+++ Algorithm task num1 c#/Algorithm task/Program.cs
@@ -0,0 +1,14 @@
+﻿//task1
+int a = 51;
+int b = 55;
+int c = a + b;
+
+if ((a > 50) && (b > 50))
+{
+    c = a + b;
+    Console.WriteLine(c);
+}
+else
+{
+    Console.WriteLine("uygun deyil");
+}
---
+++ Algorithm task num2 c#/Algotask2/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+namespace task2 
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n, cem = 0, i, j, k, l;
+            Console.WriteLine("Massiv elementlerinin sayini yazin");
+            n = Int16.Parse(Console.ReadLine());
+            int[] massiv = new int[n];
+            Console.WriteLine("Massiv elementlerini sirayla alt-alta yazin");
+            for (i = 0; i < n; i++)
+            {
+                massiv[i] = Int16.Parse(Console.ReadLine());
+                
+            }
+            
+            for (j = 0; j < n; j++)
+            {
+                for (k = 0; k < n; k++)
+                {
+                    
+                    if (massiv[j] == massiv[k])
+                    {
+                        
+                        
+                        for (l = 0; l < j; l++)
+                        {
+                            
+                            if (massiv[l] == massiv[j])
+                                
+                                cem = -1;
+                        }
+                        cem++;
+                    }
+                }
+                if (cem != 0)
+                    Console.WriteLine(massiv[j] + " regemi " + cem + " defe tekrar olunub");
+                cem = 0;
+            }
+            Console.ReadKey();
+        }
+    }
+}
